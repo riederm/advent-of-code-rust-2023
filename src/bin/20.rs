de94809519bc4d678283da15978167b512bc57fc
Mathias Rieder <mathias.rieder@gmail.com>
2023-12-23 20:25:52 +0100

    day20

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,4 +1,7 @@
-use std::{collections::{HashMap, VecDeque}, ops::Not};
+use std::{
+    collections::{HashMap, VecDeque},
+    ops::Not,
+};
 
 use itertools::Itertools;
 
@@ -7,7 +10,7 @@ advent_of_code::solution!(20);
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Module {
     FlipFlop(bool),
-    Conjunction(HashMap<String, bool>),
+    Conjunction(String, HashMap<String, bool>, Option<bool>),
     Broadcaster(),
 }
 
@@ -25,7 +28,11 @@ fn parse(input: &str) -> (HashMap<String, Module>, HashMap<String, Vec<String>>)
             ),
             name if name.starts_with('&') => (
                 name.trim_start_matches('&').to_string(),
-                Module::Conjunction(HashMap::new()),
+                Module::Conjunction(
+                    name.trim_start_matches('&').to_string(),
+                    HashMap::new(),
+                    Some(false),
+                ),
             ),
             _ => {
                 dbg!(&l);
@@ -41,12 +48,11 @@ fn parse(input: &str) -> (HashMap<String, Module>, HashMap<String, Vec<String>>)
 
     for (name, connected) in &adjacencies {
         for m in connected {
-            if let Some(Module::Conjunction(state)) = modules.get_mut(m) {
+            if let Some(Module::Conjunction(_, state, _)) = modules.get_mut(m) {
                 state.insert(name.to_string(), false);
             }
         }
     }
-
 
     (modules, adjacencies)
 }
@@ -70,7 +76,9 @@ impl Counter {
 fn simulate(
     modules: &mut HashMap<String, Module>,
     adjacencies: &HashMap<String, Vec<String>>,
-    counter: &mut Counter
+    counter: &mut Counter,
+    i: u32,
+    memory: &mut HashMap<&str, Vec<u32>>,
 ) -> u32 {
     // from, to, signal
     let mut pending: VecDeque<(&str, &str, bool)> = VecDeque::new();
@@ -80,7 +88,6 @@ fn simulate(
     while let Some((from, to, sig)) = pending.pop_front() {
         // println!("{} -{} -> {}", from, sig, to);
         if let Some(module) = modules.get_mut(to) {
-            
             let connected = adjacencies.get(to).expect(to);
             match module {
                 Module::FlipFlop(state) if !sig => {
@@ -90,63 +97,126 @@ fn simulate(
                     }
                     counter.count(state, connected.len());
                 }
-                Module::Conjunction(state) => {
+                Module::Conjunction(name, state, output) => {
                     if let Some(memory) = state.get_mut(from) {
                         *memory = sig;
-                    }else{
+                    } else {
                         unreachable!()
                     }
                     let next_sig = state.values().all(|it| *it).not();
+
+                    match name.as_str() {
+                        "rz" | "kv" | "jg" | "mr" if next_sig => {
+                            if let Some(existing) = memory.get_mut(name.as_str()) {
+                                existing.push(i);
+                            }
+                        }
+                        _ => {}
+                    }
+
+                    output.replace(next_sig);
                     for t in connected {
                         pending.push_back((to, t, next_sig));
-                        
                     }
                     counter.count(&next_sig, connected.len());
                 }
                 Module::Broadcaster() => {
-                    connected.iter().for_each(|it| pending.push_back((to, it, sig)));
+                    connected
+                        .iter()
+                        .for_each(|it| pending.push_back((to, it, sig)));
                     counter.count(&sig, connected.len());
                 }
-                _ => {}//println!("IGNORED {} -> {} = {}", from, to, sig),
+                _ => {} //println!("IGNORED {} -> {} = {}", from, to, sig),
             }
-        }else if to == "rx" && !sig {
+        } else if to == "rx" && !sig {
             lows_to_rx += 1;
         }
     }
     lows_to_rx
 }
 
+fn mermaid(adjacencies: &HashMap<String, Vec<String>>) -> String {
+    let mut result = String::new();
+    for (name, connected) in adjacencies {
+        for m in connected {
+            result.push_str(&format!("{} --> {}\n", name, m));
+        }
+    }
+    result
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let (mut modules, adjacencies) = parse(input);
     let mut counter = Counter { lows: 0, highs: 0 };
-    (0..1000).for_each(|i| {
-        simulate(&mut modules, &adjacencies, &mut counter);
-        // dbg!(&modules);
-        // println!("-------------------");
+    (0..1000).for_each(|_| {
+        simulate(
+            &mut modules,
+            &adjacencies,
+            &mut counter,
+            0,
+            &mut HashMap::new(),
+        );
     });
-    // dbg!(&counter);
     Some(counter.highs as u64 * counter.lows as u64)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let (mut modules, adjacencies) = parse(input);
-    let mut counter = Counter { lows: 0, highs: 0 };
-    let mut rx = 0;
-    let mut runs = 0u32;
-    let (mut min, mut max) = (0, 0);
-    while rx != 1 {
-        rx = simulate(&mut modules, &adjacencies, &mut counter);
-        runs += 1;
-        
-        for m in modules.values() {
-            
+fn lcm(elements: Vec<u32>) -> u64 {
+    let mut elements = elements;
+    let mut result = 1;
+    let mut divisor = 2;
+
+    loop {
+        let mut divisible = false;
+        for i in 0..elements.len() {
+            if elements[i] % divisor == 0 {
+                divisible = true;
+                elements[i] = elements[i] / divisor;
+            }
         }
 
-        
-        println!("-------------------");
+        if divisible {
+            result = result * (divisor as u64);
+        } else {
+            divisor += 1;
+        }
 
+        if elements.iter().all(|it| *it == 1) {
+            return result;
+        }
     }
-    Some(runs)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let (mut modules, adjacencies) = parse(input);
+
+    // print!("{}", mermaid(&adjacencies));
+    let mut counter = Counter { lows: 0, highs: 0 };
+
+    let mut conjunctions: HashMap<&str, Vec<u32>> = HashMap::from_iter([
+        ("rz", vec![]),
+        ("kv", vec![]),
+        ("jg", vec![]),
+        ("mr", vec![]),
+    ]);
+
+    for i in 1..100000 {
+        simulate(
+            &mut modules,
+            &adjacencies,
+            &mut counter,
+            i,
+            &mut conjunctions,
+        );
+
+        if conjunctions.values().all(|it| it.len() >= 2) {
+            break;
+        }
+    }
+
+    dbg!(&conjunctions);
+    let steps = conjunctions.values().map(|it| it[1] - it[0]).collect_vec();
+
+    Some(lcm(steps))
 }
 
 #[cfg(test)]
